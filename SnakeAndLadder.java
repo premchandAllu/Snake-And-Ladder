334ef0f0894bc88928cc701ca6cc450098e2728c
premchandAllu <premchandallu1999@gmail.com>
2020-09-25 09:21:59 +0530

    Snake And Ladder game is Started

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.Bridgelabz.SnakeAndLadder;
+
+public class SnakeAndLadder 
+  { 
+	public static void main(String[] args)
+	  {
+		//Printing Output
+		System.out.println("Welcome to Snake and Ladder Game");
+	  }
+  }
+ 
