99821ff03c72e2d7e96e40c66a89490f7ed1320b
premchandAllu <premchandallu1999@gmail.com>
2020-09-25 10:08:00 +0530

    added: Option for Ladder,Snake,No Play is added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,12 +2,40 @@ package com.Bridgelabz.SnakeAndLadder;
 
 public class SnakeAndLadder 
   { 
+	  // Constants
+	  public static final int LADDER =1;
+	  public static final int SNAKE  =2;
+	  public static final int NO_PLAY=3;
+	
 	public static void main(String[] args)
-	  {
-		 // Generating random number
-		 int random = (int) ( (Math.floor(Math.random () * 10 ) % 6) +1 );
-		 
-	     System.out.println("Generated random number is "+random);
+	  { 
+			 //Variables
+			 int position=0;
+			 int random;
+			 int choice;
+			 
+			 random = (int) ( (Math.floor(Math.random() *10 ) % 6) +1 );
+			 choice = (int) ( Math.floor(Math.random() *10 ) % 3) +1;
+			 
+			 switch (choice)
+			 {
+			   case LADDER :
+			      {  position = position + random ;
+				     System.out.println("He got Ladder and his position is "+position);
+				     break; 
+			      }
+			   case SNAKE :
+			      {  position = position - random ;
+			         if(position<0)
+			         { position =0; }
+			         System.out.println("He got Snake and his position is "+position);
+			         break ;
+			      }
+			   case NO_PLAY :
+			      {  System.out.println("No play and his position is "+position);
+			         break ; 
+			       }
+			 }
 
 	  }
   }
