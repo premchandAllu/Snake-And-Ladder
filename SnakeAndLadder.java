d5fd5bf5a8bd725f34fbdb7a027923e96173942f
premchandAllu <premchandallu1999@gmail.com>
2020-09-25 11:43:12 +0530

    Added: Two players game is added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,46 +7,68 @@ public class SnakeAndLadder
 	   public static final int SNAKE  =2;
        public static final int NO_PLAY=0;
        
-    public static void main(String[] args)
-    {
-	    //Variables
-	    int position=0;
-	    int count =0;
-	    int random;
-	    int choice;
-	 
-        while(position>=0 && position<100)
-         {
-	       random = (int) ( (Math.floor(Math.random() *10 ) % 6) +1 );
-	       choice = (int) ( Math.floor(Math.random() *10 ) % 3) +1;
-	     
-	          switch (choice)
-	           {
-	            case LADDER :
-	            {  
-		         position = position + random;
-			        break; }
-	            case SNAKE :
-	            {  
-            	  position = position - random;
-            	    if(position<0)
-            	     { position =0;} 
-	              break ;
-	              }
-	            case NO_PLAY:
-	             break ;
-	            }
-	          int number= random;
-	          if(position>100)
-	           { position=position-number;}
-	            
-	          count++;
-	    
-               if(position==100)
-                { 
-   	              System.out.println("Player has won the game and his final position is "+position);
-   	              System.out.println("No of times dice rolled is "+count);}
-                  
-         }	
-    }
+    	public static void main(String[] args) 
+    	{
+    		// variable
+    		int p1  = 0;
+    		int p2  = 0;
+    		int cur = 0;
+
+    		int random = (int) ((Math.floor(Math.random() * 10) % 6) + 1);
+
+    		// Computation
+    		if (p1 < 0) {
+    			p1 = 0;
+    		}
+    		if (p2 < 0) {
+    			p2 = 0;
+    		}
+    		while (p1 != 100 && p2 != 100) {
+    			if (cur % 2 == 0) {
+    				cur = p1;
+    				random = (int) (Math.random() * (6) + 1);
+    				int choice = (int) (Math.random() * (3) + 1);
+    				switch (choice) {
+    				case NO_PLAY:
+    					break;
+    				case LADDER:
+    					p1 = p1 + random;
+    					do {
+    						random = (int) (Math.random() * (6) + 1);
+    						p1 += random;
+    					} while (choice != LADDER);
+    					break;
+    				case SNAKE:
+    					p1 = p1 - random;
+    					break;
+    				}
+    			} else {
+    				cur = p2;
+    				random = (int) (Math.random() * (6) + 1);
+    				int choice = (int) (Math.random() * (3) + 1);
+    				switch (choice) {
+    				case NO_PLAY:
+    					break;
+    				case LADDER:
+    					p2 = p2 + random;
+    					do {
+    						random = (int) (Math.random() * (6) + 1);
+    						p2 += random;
+    					} while (choice != LADDER);
+    					break;
+    				case SNAKE:
+    					p2 = p2 - random;
+    					break;
+    				}
+    			}
+    			cur++;
+    		}
+    		if (p1 > p2) {
+    			System.out.println("Player 1 is winner");
+    		} else if (p1 < p2) {
+    			System.out.println("Player 2 is winner");
+    		} else {
+    			System.out.println("Player 1 and Player 2 are winners");
+    		}
+    	}
 }
